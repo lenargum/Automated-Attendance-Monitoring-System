d72dd43968b7045c76bdf623fd9d1cbf8fcedb99
Lgmrszd <m.v.bobrov@yandex.ru>
2019-03-18 21:53:26 +0300

    Move sqlalchemy config to config.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config.from_object(Config)
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from app import routes
