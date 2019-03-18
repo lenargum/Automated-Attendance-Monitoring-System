9f536edc5fb0e0948abd713d67bf390b16c4e80b
Lgmrszd <m.v.bobrov@yandex.ru>
2019-03-18 20:16:42 +0300

    basic html pages view

--- app/routes.py
+++ app/routes.py
@@ -1,22 +1,23 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    return render_template("index.html")
 
 
 @app.route("/home")
 def home():
-    return "Home page test"
+    return render_template("home.html")
 
 
 @app.route("/qrcode_generate")
 def qr_code_generate():
-    return "QRCode generation test"
+    return render_template("qrcode_generate.html")
 
 
 @app.route("/qrcode/<token>")
 def qr_code_token(token):
-    return "Token test: {}".format(token)
+    return render_template("qrcode_token.html", token=token)
