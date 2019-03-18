2e0e0ff1022584e63190f20d3f73e07089abeaf6
rwahog <rwahog@gmail.com>
2019-03-18 21:21:39 +0300

    create student model

---
+++ models.py
@@ -0,0 +1,17 @@
+from app import db
+
+class Person(db.Model):
+    __tablename__ = 'students'
+    
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String())
+    surname = db.Column(db.String())
+    date = db.Column(db.String())
+    
+    def __init__(self, name, surname, date):
+        self.name = name
+        self.surname = surname
+        self.date = date
+    
+    def __repr__(self):
+        return '%s %s attend lecture at %s' % (self.name, self.surname, self.date)
