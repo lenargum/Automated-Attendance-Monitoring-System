4a7666c5a550699f50ae63ffb5e30780198bbdf2
Lgmrszd <m.v.bobrov@yandex.ru>
2019-03-18 21:01:11 +0300

    forms & stuff

---
+++ app/forms.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember_me = BooleanField("Remember Me")
+    submit = SubmitField("Sign In")
+
+
+class TokenConfirmForm(FlaskForm):
+    group_name = StringField("BS Group name", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    last_name = StringField("Last name", validators=[DataRequired()])
+    submit = SubmitField("Check")
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,12 @@
 from flask import render_template
 from app import app
+from app.forms import TokenConfirmForm
 
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("index.html")
-
-
-@app.route("/home")
-def home():
-    return render_template("home.html")
 
 
 @app.route("/qrcode_generate")
@@ -20,4 +16,5 @@ def qr_code_generate():
 
 @app.route("/qrcode/<token>")
 def qr_code_token(token):
-    return render_template("qrcode_token.html", token=token)
+    form = TokenConfirmForm()
+    return render_template("qrcode_token.html", token=token, form=form)
