bfe8d1bd3bc81ef57ee44db8a696a5148ae9d074
Lgmrszd <m.v.bobrov@yandex.ru>
2019-04-01 01:16:07 +0300

    import fix

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from uuid import uuid4
 from app import db
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from app import login
 
 @login.user_loader
