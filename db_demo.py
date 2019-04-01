19c79264c48e8b6a832b157dd4026fa5240f9480
Lgmrszd <m.v.bobrov@yandex.ru>
2019-04-02 00:04:11 +0300

    user adding script update

--- db_demo.py
+++ db_demo.py
@@ -1,14 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
 from app import db
 from app import models
 
-print("Users filename: ")
-filename = input()
-for person in open(filename, "r").readlines():
-    data = person.strip().split(" ")
-    u = models.User(name=data[0], surname=data[1], email=data[2], is_faculty=False)
+print(sys.argv)
+
+if len(sys.argv) != 2:
+    print("Usage: {} INPUT".format(sys.argv[0]))
+    sys.exit(1)
+
+try:
+    with open(sys.argv[1], "r") as f:
+        lines = f.readlines()
+except FileNotFoundError as e:
+    print("{}: file not found".format(sys.argv[1]))
+    sys.exit(1)
+
+for line in lines:
+    data = line.strip().split(" ")
+    name = data[0]
+    surname = data[1]
+    email = data[2]
+    if models.User.query.filter_by(email=email).first():
+        print("Cannot add user {} {} ({}): email already in database".format(name, surname, data))
+        continue
+    u = models.User(name=name, surname=surname, email=email, is_faculty=False)
     u.set_password("test")
     db.session.add(u)
-try:
     db.session.commit()
-except Exception:
-    print("[Error] DB already has this users.\nReverting changes...")
+    print("Added user {} {} ({})".format(name, surname, data))
