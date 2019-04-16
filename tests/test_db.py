0ee0c5eee7f5cad158923958662b93ccdf29135d
Egor Polivtsev <letsbattle@yandex.ru>
2019-04-16 09:34:54 +0300

    add tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 def test_new_db():
     new_user = models.User(id=1001, name="Sample", surname="Man",
-                           email="sample@sample.com", password_hash="123456", is_faculty=False)
+                           email="sample@sample.com", password_hash="123456", is_admin=False)
     db.session.add(new_user)
     db.session.commit()
 
@@ -14,4 +14,4 @@ def test_new_db():
     assert user.surname == "Man"
     assert user.email == "sample@sample.com"
     assert user.password_hash == "123456"
-    assert user.is_faculty == False
+    assert user.is_admin == False
