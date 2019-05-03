13caab63f434b417b251b87ed76bc3917c65ce3f
rwahog <rwahog@gmail.com>
2019-05-03 16:10:34 +0300

    Update populate script

--- populate_sample_data.py
+++ populate_sample_data.py
@@ -22,10 +22,12 @@ roles = [
 ]
 
 teachers = [
-        {"name": "Vladimir", "surname": "Ivanov", "email": "v.ivanov@innopolis.ru", "password_hash": "pbkdf2:sha256:150000$7pCwLTDU$e954fc42bb4e69affa7cb4a1c8adf4ceb5afff22089c11aaea51c2b1bd70bea4", "is_admin": "false"}]
+        {"name": "Vladimir", "surname": "Ivanov", "email": "v.ivanov@innopolis.ru", "password_hash": "pbkdf2:sha256:150000$7pCwLTDU$e954fc42bb4e69affa7cb4a1c8adf4ceb5afff22089c11aaea51c2b1bd70bea4", "is_admin": 0},
+        {"name": "Ivan", "surname": "Vladimirov", "email": "i.kalinin@innopolis.ru", "password_hash": "pbkdf2:sha256:150000$7pCwLTDU$e954fc42bb4e69affa7cb4a1c8adf4ceb5afff22089c11aaea51c2b1bd70bea4", "is_admin": 0}]
+
 
 print("Adding Admin")
-u = models.User(name="Admin", surname="Admin", email="none@mail", is_admin=True)
+u = models.User(name="Admin", surname="Admin", email="admin@innopolis.ru", is_admin=True)
 u.set_password(input("Enter Admin password: "))
 db.session.add(u)
 db.session.commit()
@@ -50,7 +52,35 @@ for course in courses:
 
 print("Adding roles")
 for role in roles:
+    print("Adding {}...".format(role["name"]))
     r = models.Role(**role)
     db.session.add(r)
     db.session.commit()
     print("Added")
+
+print("Adding TAs")
+for ta in teachers:
+    print("Adding {} {}...".format(ta["name"], ta["surname"]))
+    t = models.User(**ta)
+    db.session.add(t)
+    db.session.commit()
+    print("Added")
+
+print("Assigning TAs to the courses")
+ta_id = 1
+for ta in teachers:
+    ta_id += 1
+    course_id = 1
+    for course in courses:
+        print("Assigning {} {} to the {}...".format(ta["name"], ta["surname"], course["name"]))
+        enrollment = {}
+        enrollment["user"] = ta_id
+        enrollment["course"] = course_id
+        enrollment["role"] = 2
+        print(enrollment)
+        e = models.Enrollment(**enrollment)
+        db.session.add(e)
+        db.session.commit()
+        print("Added")
+        course_id += 1
+
